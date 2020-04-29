6962820f29a711bf685e28bab3411c218611d0bc
srfrog <shawn.hayes70@gmail.com>
2020-04-28 20:07:45 -0500

    Added route for heroku

--- routes/api.js
+++ routes/api.js
@@ -1,32 +1,37 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-router.post("/api/transaction", ({body}, res) => {
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/htmlFile.html"));
+});
+
+router.post("/api/transaction", ({ body }, res) => {
   Transaction.create(body)
-    .then(dbTransaction => {
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(404).json(err);
     });
 });
 
-router.post("/api/transaction/bulk", ({body}, res) => {
+router.post("/api/transaction/bulk", ({ body }, res) => {
   Transaction.insertMany(body)
-    .then(dbTransaction => {
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(404).json(err);
     });
 });
 
 router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
-    .then(dbTransaction => {
+  Transaction.find({})
+    .sort({ date: -1 })
+    .then((dbTransaction) => {
       res.json(dbTransaction);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(404).json(err);
     });
 });
